9b3daa82081047bb16dff3a9ae7089963217cfd0
Santiago Guastoni <sguastoni@hotmail.com>
2023-05-24 13:46:36 -0300

    cambiado forma que se valida en reserva

--- scripts_js/val_reserva.js
+++ scripts_js/val_reserva.js
@@ -1,3 +1,10 @@
+function sendSubmit()
+{
+    //aquí se comunicaría con backend y espera respuesta. 
+    //como no hay backend muestro mensaje y permito el submit.
+    alert("Su reserva fue procesada. Gracias! vuelva pronto.");
+    return true;
+}
 function changeCantEntradas() 
 {
     let total = document.forms["form_reserva"]["cantidad_entradas"].value * 1500;
