dc35ee1a6668a69506fcf27a778887873695caa2
Agustin Rodriguez <rodriguez.amr98@gmail.com>
2023-07-12 00:00:32 -0300

    Se envía el formulario de reserva

--- scripts_js/val_reserva.js
+++ scripts_js/val_reserva.js
@@ -1,12 +1,18 @@
 function sendSubmit()
 {
-    //aquí se comunicaría con backend y espera respuesta. 
+    //aquí se comunicaría con backend y espera respuesta.
     //como no hay backend muestro mensaje y permito el submit.
-    alert("Su reserva fue procesada. Gracias! vuelva pronto.");
-    return true;
+    // alert("Su reserva fue procesada. Gracias! vuelva pronto.");
+    // return true;
 }
 function changeCantEntradas() 
 {
     let total = document.forms["form_reserva"]["cantidad_entradas"].value * 1500;
     document.getElementById("total_pelicula").innerHTML = "Total: $" + total;
+}
+function actualDate() {
+    const today = new Date().toLocaleDateString('fr-ca');
+    const date = document.getElementById("dia_pelicula");
+    date.setAttribute("min", today);
+    date.defaultValue = today;
 }
